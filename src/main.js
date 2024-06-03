5d0bbe47aded8afe968f25e360454221cc94331f
vladakolos <linikvladislava@gmail.com>
2024-06-03 10:42:32 +0800

    footer created
    html,css,js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import review from "./js/reviews";
-import { initializeSlider } from "./js/slider";
-
+import review from './js/reviews';
+import { initializeSlider } from './js/slider';
+import { blackFooter } from './js/blackFooter';
 initializeSlider();
 review();
+blackFooter();
